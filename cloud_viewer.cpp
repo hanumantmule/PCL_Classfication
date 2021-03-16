2cc28c3213a4a07fea13a993e994d05e5d0e6614
Hanumant <mule.hanumant@gmail.com>
2021-03-17 00:04:44 +0530

    Add project files.

---
+++ cloud_viewer.cpp
@@ -0,0 +1,148 @@
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/shot.h>
+#include <pcl/keypoints/iss_3d.h>
+
+double
+computeCloudResolution(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
+{
+	double resolution = 0.0;
+	int numberOfPoints = 0;
+	int nres;
+	std::vector<int> indices(2);
+	std::vector<float> squaredDistances(2);
+	pcl::search::KdTree<pcl::PointXYZ> tree;
+	tree.setInputCloud(cloud);
+
+	for (size_t i = 0; i < cloud->size(); ++i)
+	{
+		if (!pcl_isfinite((*cloud)[i].x))
+			continue;
+
+		// Considering the second neighbor since the first is the point itself.
+		nres = tree.nearestKSearch(i, 2, indices, squaredDistances);
+		if (nres == 2)
+		{
+			resolution += sqrt(squaredDistances[1]);
+			++numberOfPoints;
+		}
+	}
+	if (numberOfPoints != 0)
+		resolution /= numberOfPoints;
+
+	return resolution;
+}
+
+int iss_detector()
+{
+	// Objects for storing the point cloud and the keypoints.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints(new pcl::PointCloud<pcl::PointXYZ>);
+
+	// Read a PCD file from disk.
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("bunny.pcd", *cloud) != 0)
+	{
+		return -1;
+	}
+
+	// ISS keypoint detector object.
+	pcl::ISSKeypoint3D<pcl::PointXYZ, pcl::PointXYZ> detector;
+	detector.setInputCloud(cloud);
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZ>);
+	detector.setSearchMethod(kdtree);
+	double resolution = computeCloudResolution(cloud);
+	// Set the radius of the spherical neighborhood used to compute the scatter matrix.
+	detector.setSalientRadius(2 * resolution);
+	// Set the radius for the application of the non maxima supression algorithm.
+	detector.setNonMaxRadius(2 * resolution);
+	// Set the minimum number of neighbors that has to be found while applying the non maxima suppression algorithm.
+	detector.setMinNeighbors(4);
+	// Set the upper bound on the ratio between the second and the first eigenvalue.
+	detector.setThreshold21(0.975);
+	// Set the upper bound on the ratio between the third and the second eigenvalue.
+	detector.setThreshold32(0.975);
+	// Set the number of prpcessing threads to use. 0 sets it to automatic.
+	detector.setNumberOfThreads(4);
+
+	detector.compute(*keypoints);
+
+	std::cout << " ISS keypoint estimated";
+	std::cout << " with size " << keypoints->size() << std::endl;
+
+	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+	// Object for storing the SHOT descriptors for each point.
+	pcl::PointCloud<pcl::SHOT352>::Ptr descriptors(new pcl::PointCloud<pcl::SHOT352>());
+
+	// Estimate the normals.
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
+	normalEstimation.setInputCloud(keypoints);
+	normalEstimation.setRadiusSearch(0.03);
+	normalEstimation.setSearchMethod(kdtree);
+	normalEstimation.compute(*normals);
+
+	// SHOT estimation object.
+	pcl::SHOTEstimation<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> shot;
+	shot.setInputCloud(keypoints);
+	shot.setInputNormals(normals);
+	// The radius that defines which of the keypoint's neighbors are described.
+	// If too large, there may be clutter, and if too small, not enough points may be found.
+	shot.setRadiusSearch(0.50);
+
+	shot.compute(*descriptors);
+
+
+	std::cout << " SHOT feature estimated";
+	std::cout << " with size " << descriptors->size() << std::endl;
+	return 0;
+}
+
+int shot_descriptor()
+{
+	// Object for storing the point cloud.
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	// Object for storing the normals.
+	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+	// Object for storing the SHOT descriptors for each point.
+	pcl::PointCloud<pcl::SHOT352>::Ptr descriptors(new pcl::PointCloud<pcl::SHOT352>());
+
+	// Read a PCD file from disk.
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("bunny.pcd", *cloud) != 0)
+	{
+		return -1;
+	}
+
+	// Note: you would usually perform downsampling now. It has been omitted here
+	// for simplicity, but be aware that computation can take a long time.
+
+	// Estimate the normals.
+	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normalEstimation;
+	normalEstimation.setInputCloud(cloud);
+	normalEstimation.setRadiusSearch(0.03);
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZ>);
+	normalEstimation.setSearchMethod(kdtree);
+	normalEstimation.compute(*normals);
+
+	// SHOT estimation object.
+	pcl::SHOTEstimation<pcl::PointXYZ, pcl::Normal, pcl::SHOT352> shot;
+	shot.setInputCloud(cloud);
+	shot.setInputNormals(normals);
+	// The radius that defines which of the keypoint's neighbors are described.
+	// If too large, there may be clutter, and if too small, not enough points may be found.
+	shot.setRadiusSearch(0.50);
+
+	shot.compute(*descriptors);
+
+
+	std::cout << " SHOT feature estimated";
+	std::cout << " with size " << descriptors->size() << std::endl;
+
+	return 0;
+}
+
+int
+main(int argc, char** argv)
+{
+		
+	iss_detector();
+	//shot_descriptor();
+}
